ca62a71824d7a53605efc618ad3b932d69ae449d
BarnetteME1 <BarnetteME1@gmail.com>
2015-11-05 19:15:51 -0500

    added models and files

--- adventure_time/atus/admin.py
+++ adventure_time/atus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Respondant)
+admin.site.register(Activities)
+admin.site.register(Household)
+admin.site.register(ActivityCode)
--- adventure_time/atus/models.py
+++ adventure_time/atus/models.py
@@ -5,12 +5,21 @@ from django.db import models
 
 class Respondant(models.Model):
     case_id = models.CharField(max_length=14)
+    weight = models.FloatField()
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1)
 
 
-class Activity(models.Model):
-    pass
+class Activities(models.Model):
+    case_id = models.ManyToMany(Respondant)
 
 
 class Household(models.Model):
     pass
 
+
+class ActivityCode(models.Model):
+    code = models.IntegerField()
+    title = models.CharField(max_length=50)
+    average_minutes = models.IntegerField()
+    total_respondants = models.IntegerField()
